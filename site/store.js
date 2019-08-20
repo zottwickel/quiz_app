29b75fe58c5439995812b693daa95d77c64a39e5
zottwickel <scott.g.mckell@gmail.com>
2019-08-20 16:03:58 -0600

    add wireframe and site files

---
+++ site/store.js
@@ -0,0 +1,113 @@
+const STORE = [
+    {
+        id: 1,
+        question: "What type of bicycle should you use to commute?",
+        answers: [
+            "Full suspension mountain bike.",
+            "E-bike.",
+            "Hybrid on-road/off-road bike.",
+            "Whatever bike you're comfortable with."
+        ],
+        correct: 3,
+    },
+    {
+        id: 2,
+        question: "What types of weather should you ride in?",
+        answers: [
+            "Sunshine.",
+            "Darkness.",
+            "Rain.",
+            "Snow.",
+            "All of the above"
+        ],
+        correct: 4,
+    },
+    {
+        id: 3,
+        question: "What tools do you need?",
+        answers: [
+            "Bell, horn, and other noise makers.",
+            "The entire workshop.",
+            "Spandex suit, reflective vest, race numbers.",
+            "Tire levers, spare tube, multi-tool and an air pump."
+        ],
+        correct: 3,
+    },
+    {
+        id: 4,
+        question: "Where is it legal to ride?",
+        answers: [
+            "Anywhere a car can drive, plus bike lanes.",
+            "Sidewalks and bridges.",
+            "As far to the right as you can get and nowhere else.",
+            "Down the median."
+        ],
+        correct: 0
+    },
+    {
+        id: 5,
+        question: "How often should you get your bike checked?",
+        answers: [
+            "Every week.",
+            "Every month.",
+            "Every 6 months.",
+            "Every year."
+        ],
+        correct: 2
+    },
+    {
+        id: 6,
+        question: "When should you use your bike lights?",
+        answers: [
+            "All the time.",
+            "When you want to be seen better.",
+            "Nighttime only.",
+            "Never."
+        ],
+        correct: 1,
+    },
+    {
+        id: 7,
+        question: "How should you treat other cyclists?",
+        answers: [
+            "RACE THEM EVERYWHERE!",
+            "Avoid them at all costs.",
+            "Reach out and be friendly - it's a jungle out there.",
+            "Imagine they don't exist."
+        ],
+        correct: 2
+    },
+    {
+        id: 8,
+        question: "What type of bike lock is best?",
+        answers: [
+            "No lock needed, bikes are never stolen.",
+            "A U-lock and possibly a cable lock for the tires.",
+            "A combination lock.",
+            "Chain and padlock."
+        ],
+        correct: 1
+    },
+    {
+        id: 9,
+        question: "How should you signal to drivers?",
+        answers: [
+            "With your middle finger.",
+            "Point where you are about to go unless unsafe to do so.",
+            "No signaling is needed.",
+            "With clothes that say \"I'm cool\"."
+        ],
+        correct: 1
+    },
+    {
+        id: 10,
+        question: "Bicycle commuting is:",
+        answers: [
+           "A great way to stay fit.",
+           "A way of life.",
+           "Road-rage free.",
+           "All of the above."
+        ],
+        correct: 3
+    }
+]
